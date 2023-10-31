8fa2cc33aa37340e45e7b8f060d117c75e333268
Sarah Clark <sarahireneclark@gmail.com>
2023-10-30 19:00:23 -0500

    Initial commit of project

---
+++ DigitalCrownGame/DigitalCrownGame Watch App/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  DigitalCrownGame Watch App
+//
+//  Created by Sarah Clark on 10/30/23.
+//
+
+import SpriteKit
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DigitalCrownGame/DigitalCrownGame Watch App/DigitalCrownGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigitalCrownGameApp.swift
+//  DigitalCrownGame Watch App
+//
+//  Created by Sarah Clark on 10/30/23.
+//
+
+import SwiftUI
+
+@main
+struct DigitalCrownGame_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
